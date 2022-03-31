63dc451b99a90aada887eefe698fada93eafd422
thmcpython <60908281+thmcpython@users.noreply.github.com>
2022-03-30 17:00:11 -0700

    Heroku chrome buildpack env option added

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from bokeh.embed import file_html
 from bokeh.plotting import figure, output_file, save
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.binary_location = r"/app/.apt/usr/bin/google-chrome"
+chrome_options.binary_location = os.getenv('GOOGLE_CHROME_BIN')
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
